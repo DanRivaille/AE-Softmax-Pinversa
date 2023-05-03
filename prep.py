d43fa7e26c0c7a9c4b9345a5884aaed692671ecd
Dan Santos <dan.santos.hd@outlook.com>
2023-05-02 21:42:52 -0400

    Se crea la base del archivo prep

---
+++ prep.py
@@ -0,0 +1,21 @@
+import numpy as np
+import utility as ut
+
+# Save data: training and testing
+def save_data():
+  pass
+
+# Binary label
+def load_data_csv():
+  pass
+
+# Configuration of the SAEs
+def load_config():
+  pass
+
+# Beginning ...
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
